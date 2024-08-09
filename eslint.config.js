b366b65e857e288d1c039cfaf496648925740ca0
General_K1ng <General_K1ng@outlook.com>
2024-08-09 20:01:34 +0800

    first commit (expo): Initialize music player project and configure environment
    Created a new Expo project named "music-player" and set up the project structure and environment settings. This includes defining the project version, configuring orientation, icons, user interface styles, and splash screens. Platform-specific configurations for iOS and Android, such as scheme, adaptive icons, and package names, were also set up.
    
    Additionally, ESLint and Prettier were configured to ensure code quality and consistency, and React Native support was enabled in the project.
    
    BREAKING CHANGE: This commit initializes the project with the initial structure and configuration, affecting all subsequent development work.

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// eslint.config.js
+module.exports = {
+    // 其他 ESLint 配置
+    ignores: [
+        'node_modules/',
+        '.expo/',
+        '.next/',
+        '__generated__/',
+        'build/',
+        'react-native-lab/react-native/',
+        'docs/react-native-website/',
+        'android/',
+        'assets/',
+        'bin/',
+        'fastlane/',
+        'ios/',
+        'kotlin/providers/',
+        'vendored/',
+        'docs/public/static/',
+    ],
+    // 其他 ESLint 配置
+};
